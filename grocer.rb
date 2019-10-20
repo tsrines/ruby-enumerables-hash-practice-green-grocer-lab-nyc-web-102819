d2876067b792049ff4b77a778e1ff5bb0feba7e0
Timothy Rines <tsrines@gmail.com>
2019-10-20 00:59:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -54,4 +54,4 @@ def checkout(cart, coupons)
     total += cart_with_discounts_applied[item][:price]*cart_with_discounts_applied[item][:count]
   end
   total > 100.00 ? (total * 0.90).round : total
-end
+end 
